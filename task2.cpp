923dcc800fb4d455b1f5de327aa42b90c7f61ab9
phamdatHCMUT <you@example.com>
2020-12-25 21:24:39 +0700

    btl2

---
+++ task2.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Interval
+{
+public:
+    int start;
+    int end;
+
+    Interval(int start = 0, int end = 0)
+    {
+        this->start = start;
+        this->end = end;
+    }
+};
+
+int minPark(vector<Interval> lInterval)
+{
+    //STUDENT_ANSWER
+}
+
+class BusParking {
+public:
+    void add(int s, int t) {
+        
+    }
+    
+    void remove(int s, int t) {
+        
+    }
+    
+    int minPark() {
+        return 0;
+    }
+};
+
+int main()
+{
+    vb->add(1, 5);
+    b->add(2, 5);
+    b->add(3, 5);
+    cout << b->minPark();
+    return 0;
+}
